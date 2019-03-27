ffcd59a14c6f10794a553ef80686b36a15a09f06
Anton Santos <43733827+antondotio@users.noreply.github.com>
2019-03-27 16:47:59 -0600

    Update MainClass.java

--- MainClass.java
+++ MainClass.java
@@ -12,12 +12,17 @@ public class MainClass{
         main.setSize(600, 400);
         p.setLayout(bl);
         options.setLayout(new FlowLayout());
-        p.add("South", options);
-    
+        p.add("South", options);  
+        p.add("Center", new JEditorPane());
+        JLabel title = new JLabel("An Application to Maintain Student Records");
+        title.setHorizontalAlignment(JLabel.CENTER);
+        p.add("North", title);
+
         options.add(new Button("Insert"));
         options.add(new Button("Find"));
         options.add(new Button("Browse"));
         options.add(new Button("Create Tree From File"));
+
         main.setContentPane(p);
         main.setVisible(true);
 
